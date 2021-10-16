3e91171a1c28dfa82a5574c94a94e5546580b0b5
Mathieu Cloutier <cloutier.mat0@gmail.com>
2021-10-16 15:18:09 -0600

    Glow color can bas changed by user

--- src/animation/colors/glow.js
+++ src/animation/colors/glow.js
@@ -1,15 +1,26 @@
+import registry from "../../store/registry.js"
+
 var logBase = 1.0 / Math.log(2.0);
 var logHalfBase = Math.log(0.5)*logBase;
 
+let colorBase = [255, 0, 0];
+
+registry.addListener('updateColor1', color => {
+	colorBase = color.slice(0, 3).map(color => color /255);
+})
+
+function smoothenColor(smoothing) {
+	return colorBase.map(color => color * smoothing )
+}
+
 export default {
-		get(n, max, Tr, Ti) {
-			if(n === max) {
-				return [0, 0, 0, 255]
-			}
-			let smoothing = 5 + n - logHalfBase - Math.log(Math.log(Tr+Ti))*logBase;
-			smoothing = Math.floor(540.0*smoothing/max);
-			return [smoothing, smoothing, smoothing, 255]
-			// if ( smoothing > 255 ) smoothing = 255;
-			// return [smoothing**2/255, smoothing / 5, smoothing * 1.5, 255];
+	get(n, max, Tr, Ti) {
+		if(n === max) {
+			return [0, 0, 0, 255]
+		}
+		let smoothing = 5 + n - logHalfBase - Math.log(Math.log(Tr+Ti))*logBase;
+		smoothing = Math.floor(360.0*smoothing/max);
+		// let smoothing = n / max * 255
+		return [...smoothenColor(smoothing), 255];
 	}
 }
