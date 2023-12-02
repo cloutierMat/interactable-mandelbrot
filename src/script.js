26b68bc38aa691888d86780418a4593c93354f65
Mathieu Cloutier <cloutier.mat0@gmail.com>
2023-12-01 18:49:24 -0700

    Initialize the help script

--- src/script.js
+++ src/script.js
@@ -3,14 +3,17 @@ import canvas from "./animation/canvas.js";
 import registry from "./store/registry.js";
 import "./controllers/menu.js";
 import "./controllers/controller.js"
+import help from "./help.js";
 
 let renderCount = 0;
 let workers = [];
 let activeWorkers = new Set();
-let assignLine = () => {};
+let assignLine = () => { };
 
 // Canvas setup
 settings.init();
+help.init()
+
 
 // Dev command to always enable max CPU thread.
 // Not to keep for live version as not everybody might like the idea of a web app asking for all of their cpu resources
@@ -22,21 +25,21 @@ function createWorkers(count) {
 	workers = [];
 	// Add workers if the total isn't on par with the new count
 	for (let i = workers.length; i < count; i++) {
-		workers.push(new Worker('src/animation/mandelbrotWorker.js', {type: 'module'}));
+		workers.push(new Worker('src/animation/mandelbrotWorker.js', { type: 'module' }));
 		// we create the listener immediately. the function assignLine will be defined in the draw function
-		workers[i].onmessage = (message)=> {
-			const {lineData, yPosition, renderId} = message.data;
+		workers[i].onmessage = (message) => {
+			const { lineData, yPosition, renderId } = message.data;
 			canvas.putImageData(lineData, yPosition);
 			assignLine(i, renderId);
 		};
 	}
 	// if the new worker count is lower than the previous count, delete the remaining ones
-	for(let i = count; i < workers.length; i++) {
+	for (let i = count; i < workers.length; i++) {
 		workers[i].terminate();
 	}
 }
 
-if(window.Worker) {
+if (window.Worker) {
 	createWorkers(settings.getWorkerCount())
 }
 
@@ -45,17 +48,17 @@ function draw() {
 	let width = canvas.getWidth();
 	let height = canvas.getHeight();
 	let centerPoint = settings.getCenterLocation();
-	let yArray = [...Array(Math.ceil(height/50)).keys()];
-	
+	let yArray = [...Array(Math.ceil(height / 50)).keys()];
+
 	renderCount++;
 	assignLine = (worker, renderId) => {
-		if(renderCount !== renderId || !workers[worker]) {
+		if (renderCount !== renderId || !workers[worker]) {
 			// Stop rendering if the user called for a new frame or our worker no longer exists
 			activeWorkers.delete(worker);
 			return;
 		}
-		if(yArray.length > 0) {
-			let lineRendered = yArray.shift()*50;
+		if (yArray.length > 0) {
+			let lineRendered = yArray.shift() * 50;
 			workers[worker].postMessage(
 				{
 					lineData: canvas.createImageData(50),
@@ -77,16 +80,16 @@ function draw() {
 			);
 		} else {
 			activeWorkers.delete(worker);
-			if(activeWorkers.size === 0) {
-				if(settings.getAnimate()) {
+			if (activeWorkers.size === 0) {
+				if (settings.getAnimate()) {
 					settings.updateZoomFactor();
 					requestAnimationFrame(draw);
 				}
 			}
 		}
 	}
-	
-	for(let i = 0; i < settings.getWorkerCount(); i++) {
+
+	for (let i = 0; i < settings.getWorkerCount(); i++) {
 		activeWorkers.add(i);
 		assignLine(i, renderCount);
 	}
@@ -95,16 +98,16 @@ draw()
 
 // Register a force redraw event
 registry.addListener('forceRedraw', () => {
-	if(!settings.getAnimate() && settings.getRedrawIsEnabled()) {
+	if (!settings.getAnimate() && settings.getRedrawIsEnabled()) {
 		draw()
 	}
 })
 
 // Register an event to restart the animation
 registry.addListener('updateAnimate', (animate) => {
-		if(animate) {
-			draw()
-		}
+	if (animate) {
+		draw()
+	}
 })
 
 // Change the amount of webworkers
